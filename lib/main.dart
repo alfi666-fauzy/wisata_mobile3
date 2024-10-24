f429c99917d9be26e0427e0d970e6ab60015b567
Alfi <malfifauzy@gmail.com>
2024-10-24 09:50:03 +0700

    PertemuanMembuatsplashscreen

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:aslab_travel/latihanmodul/pertemuan4.dart';
+import 'package:aslab_travel/latihanmodul/project/screens/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashScreen(),
+    ); //
+  }
+}
