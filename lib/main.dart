4a2200250c09670995de389dffe7de67fb75617d
elviramutia <elviramutiapratiwi@gmail.com>
2024-11-14 09:41:10 +0700

    p6

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:aslab_travel/latihanmodul/pertemuan4.dart';
-import 'package:aslab_travel/latihanmodul/pertemuan5.dart';
-import 'package:aslab_travel/project/screens/landing_page.dart';
-import 'package:aslab_travel/project/screens/splashscreen.dart';
+import 'package:aslab_travel/latihanmodul/pertemuan6.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +11,7 @@ class TravelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LandingPageOne(),
+      home: pertemuan6(),
     );
   }
 }
